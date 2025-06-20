ab4dfdbe48282bd673702a2a895a637b065c3024
jacyl4 <jacyl4@gmail.com>
2025-06-20 21:25:56 +0800

    微调元素大小

--- astro.config.mjs
+++ astro.config.mjs
@@ -18,7 +18,8 @@ export default defineConfig({
       },
     }),
     swup({
-      containers: ['#swup']
+      containers: ['#swup'],
+      plugins: [new SwupMorphPlugin()],
   })],
   vite: {
     plugins: [
